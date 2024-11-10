5d0c86fe1e96d9e7718629047b8661e59693ed11
Fabio Scaccabarozzi <fsvm88@gmail.com>
2024-11-10 11:48:47 +0100

    scripts: comment metadata regeneration in digests_and_cache

--- scripts/digests_and_cache.sh
+++ scripts/digests_and_cache.sh
@@ -5,4 +5,4 @@ __script_dir="$(dirname "$(dirname "$(realpath "$0")")")"
 
 find "${__script_dir}" -name "*.ebuild" -exec dirname {} + | sort -u | while read linea; do pushd $linea; for x in *.ebuild; do ebuild $x digest; done; popd; done
 
-egencache --update --repo "$(basename "${__script_dir}")"
+#egencache --update --repo "$(basename "${__script_dir}")"
