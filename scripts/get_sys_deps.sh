fe2da47c0925d8d842f957e3c02784b566cca392
Fabio Scaccabarozzi <fsvm88@gmail.com>
2025-01-12 00:59:09 +0100

    scripts: make it obvious when we have a new unsupported crate

--- scripts/get_sys_deps.sh
+++ scripts/get_sys_deps.sh
@@ -98,7 +98,7 @@ rg submodule .gitmodules | rg '"[A-Za-z0-9-].*"' -o | sed 's:"::g' | sort -u | w
         "libpulse-sys") echo 'media-libs/libpulse:0' ;;
         "pixman-sys") echo 'x11-libs/pixman:0' ;;
         *)
-            echo "unsupported crate: ${crate}"
+            echo "###################################### unsupported crate: ${crate}"
             exit 254
             ;;
         esac
